cb094bb61dbef559384036a5b884f04511af3ad7
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-06-17 06:04:56 +0200

    Fix: PricingCard , using shadcn components

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -1,47 +1,44 @@
-
 "use client";
 import React from "react";
 import { ProductPricing } from "../data/types";
 import { Button } from "./ui/button";
 import { CheckCircle } from "lucide-react";
-import clsx from "clsx";
-
+import { Card, CardContent, CardFooter } from "./ui/card";
 interface PricingProps {
     tier: ProductPricing;
     highlight?: boolean;
 }
-
-export default function PricingCard({ tier, highlight }: PricingProps) {
-
+export default function PricingCard({ tier }: PricingProps) {
     const { name, price, features } = tier;
-
     return (
-        <div
-            className={`bg-white p-6 rounded-lg shadow-md transition-transform duration-300 ${highlight ? "transform scale-105" : ""
-                }`}
-        >
-            <h3 className="text-xl font-semibold mb-4">{name}</h3>
-            <p className="text-3xl md:text-5xl font-bold mb-6">
-                <span className={clsx({ "text-secondary": highlight })}>
-                    {typeof price === 'number' ? `R${price}` : price}
-                </span>
-                {typeof price === 'number' && <span className="text-lg font-normal text-gray-600">/mo</span>}
-            </p>
-            <div className="p-6 mt-1">
-                <p className="font-bold mb-0">FEATURES</p>
-                <p className="text-foreground-accent mb-5">Everything in basic, plus...</p>
-                <ul className="space-y-4 mb-8">
-                    {features.map((feature, index) => (
-                        <li key={index} className="flex items-center">
-                            <CheckCircle className="h-5 w-5 text-white mr-2 bg-amber-300 rounded-full " />
-                            <span className="text-foreground-accent">{feature}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <Button className={clsx("w-full py-3 px-4 rounded-full transition-colors", { "bg-primary hover:bg-primary-accent": highlight, "bg-hero-background hover:bg-gray-200": !highlight })}>
-                Get Started
-            </Button>
-        </div>
+        <Card className="card flex flex-col items-start p-6 shadow-lg rounded-lg transition-transform hover:scale-105">
+            <CardContent>
+                <div className="text-xl-semibold">{name}</div>
+                <div className="text-xl-medium md:text-5xl font-bold mb-6">
+                    <span className="text-md-regular">
+                        {typeof price === 'number' ? `R${price}` : price}
+                    </span>
+                    {typeof price === 'number' && <span className="text-sm-regular">/mo</span>}
+                </div>
+                <div className="p-6 mt-1">
+                    <div className="text-sm-semibold mb-2">FEATURES</div>
+                    { /* Work on this ul and items spacing */}
+                    <ul className="space-y-4 mb-8">
+                        {features.map((feature, index) => (
+                            <li key={index} className="flex items-center">
+                                <CheckCircle className="h-5 w-5 text-white mr-2 bg-amber-300 rounded-full" />
+                                <span className="text-xs-regular">{feature}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <CardFooter className="mt-4 text-xs-regular text-gray-500">
+                    <Button>
+                        Get Started
+                    </Button>
+                </CardFooter>
+            </CardContent>
+        </Card >
     );
 }
--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -5,13 +5,13 @@ import PricingCard from "./PricingCard";
 
 export default function PricingSection() {
     return (
-        <section className="flex flex-col items-center justify-center py-16 bg-gray-50">
-            <h2 className="text-3xl font-bold text-center mb-8">Pricing</h2>
-            <p className="text-lg text-center mb-12">
+        <section className="flex flex-col pt-16 items-center gap-8 self-stretch">
+            <div className="text-xl-semibold ">Pricing</div>
+            <div className="text-lg-regular">
                 Choose the plan that fits your needs. All plans come with a 30-day money-back guarantee.
-            </p>
-            <div className="flex flex-row px-4">
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            </div>
+            <div className="flex flex-col items-center gap-8 px-8 max-w-[1280px]">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                     {tiers.map((tier, index) => (
                         <PricingCard key={tier.name} tier={tier} highlight={index === 1} />
                     ))}
