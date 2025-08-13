0cf287f19c7dff2eff751c562d137b16a163d150
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-08-13 20:11:42 +0200

    Refactor: Update PricingSection header structure and styles for improved layout and responsiveness; adjust alignment and flex properties

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -8,8 +8,10 @@ const PricingSection: React.FC = () => {
             <div className="c_pricing-container">
                 {/* Section Header */}
                 <div className="c_pricing-header">
-                    <div className="text-lg-semibold md:text-md-semibold sm:text-sm-semibold text-[var(--accent)]">Pricing</div>
-                    <h2 className="heading-md-medium md:heading-sm-medium sm:heading-xs-medium">Simple, transparent pricing</h2>
+                    <h6 className="text-lg-semibold md:text-md-semibold sm:text-sm-semibold text-[var(--accent)]">Pricing
+
+                    </h6>
+                    <h2 className="heading-md-medium md:heading-sm-medium sm:heading-xs-medium xs:heading-xs-regular">Simple, transparent pricing</h2>
                     <p className="text-sm-medium md:text-xs-medium sm:text-xs-regular ">
                         We believe Untitled should be accessible to all companies, no matter the size.
                     </p>
