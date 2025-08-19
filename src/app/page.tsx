463142bf1febefb43c255d83135f5d87de3ef91a
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2025-08-19 03:54:55 +0200

    Refactor: Add SectionDivider above Footer in Home component for improved layout consistency; remove redundant SectionDivider from Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
         <SectionDivider />
         <CTASection />
       </main>
+      <SectionDivider />
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import FooterItem from "./FooterItem";
 import { footerItems } from "../data/footer";
-import SectionDivider from "./SectionDivider";
 
 export default function Footer() {
     return (
@@ -20,9 +19,6 @@ export default function Footer() {
                     ))}
                 </div>
             </div>
-
-            {/* Divider */}
-            <SectionDivider />
 
             {/* Bottom Area */}
             <div className="_footer-bottom-container">
